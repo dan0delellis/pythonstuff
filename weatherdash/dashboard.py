41c1a06707298c47450bdfdb57b9dda574e8e6d2
Daniel De Lellis <dan.delellis@gmail.com>
2020-12-07 02:00:58 -0800

    getting started

--- weatherdash/dashboard.py
+++ weatherdash/dashboard.py
@@ -3,10 +3,22 @@
 import tkinter
 from tkinter.font import Font
 from PIL import Image, ImageTk
+fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fonts')
+
+clockTx = "88:88"
+clockSz=36
+clockFt = ImageFont.truetype(os.path.join(fontdir, 'Charcoal.ttf'), clockSz)
+
+tempTx = "188F"
+tempSz = clockSz
+tempFt = ClockFt
+
+humidTx = "100%"
 
 
 root = tkinter.Tk()
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
-print(height)
+
+
 root.mainloop()
