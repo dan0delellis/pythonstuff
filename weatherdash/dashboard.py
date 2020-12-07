bfc8cf4a7973717005aa785196b85f8638d2b1f1
Daniel De Lellis <dan.delellis@gmail.com>
2020-12-07 00:58:32 -0800

    starting a new thing because why finish something when I could start another 8)

---
+++ weatherdash/dashboard.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import tkinter
+from tkinter.font import Font
+from PIL import Image, ImageTk
+
+
+root = tkinter.Tk()
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+print(height)
+root.mainloop()
