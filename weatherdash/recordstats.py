c077b2c45c78dfa76515080796c08e5b22b2aa4d
Daniel De Lellis <dan.delellis@gmail.com>
2020-12-14 00:38:04 -0800

    Use mysql instead of redis so I can have schema.

--- weatherdash/recordstats.py
+++ weatherdash/recordstats.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
-import board
-import busio
-import adafruit_bme280
-import redis
-import time
-import sched
+import board, busio, adafruit_bme280, time, sched
 from math import floor
+import mysql.connector
+
+database = "climate"
+table = "readings"
+location = "apartment"
 
 s = sched.scheduler(time.time, time.sleep)
-
-r = redis.Redis(host='10.0.0.2', port=6379, db=9)
-expiry = 3600 * 24 * 7
-
-
+db = mysql.connector.connect(host="10.0.0.2", user="sensorsrecord", password="donkeyboner", database="climate")
+mycursor = db.cursor()
 i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 def getData():
     s.enter(3,1,getData)
-    epoc = floor(time.time())
-    r.set(f"{epoc}-temperature", bme280.temperature, ex=expiry)
-    r.set(f"{epoc}-humidity", bme280.humidity, ex=expiry)
-    r.set(f"{epoc}-pressure", bme280.pressure, ex=expiry)
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    query = f"INSERT into {table} (timestamp, location, temp, humid, pressure ) VALUES ( %s, %s, %s, %s, %s)"
+    val = (timestamp, location, bme280.temperature, bme280.humidity, bme280.pressure)
+    mycursor.execute(query, val)
+    db.commit()
+
 
 s.enter(0,1,getData)
 s.run()
