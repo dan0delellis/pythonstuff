fe9e95088beffe5a97f7d493fea25fb3b155f4ff
Daniel De Lellis <dan.delellis@gmail.com>
2020-12-15 01:33:15 -0800

    not working graph

---
+++ weatherdash/graph.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import redis
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import dates as mdates
+from matplotlib import cbook as cbook
+import mysql.connector as sql
+from datetime import timedelta, datetime
+
+datetimeString = "%Y-%m-%d %H:%M:%S"
+db = sql.connect(host="10.0.0.2", user="sensorsrecord", password="donkeyboner", database="climate")
+
+endTimeStamp = datetime.now()
+lookbackRange = 24 * timedelta(hours=1)
+startTimeStamp = endTimeStamp - lookbackRange
+
+
+matplotlib.use('TkAgg')
+
+cursor = db.cursor()
+
+sql = "Select timestamp, temp from readings where timestamp <= %s and timestamp >= %s"
+var = (endTimeStamp.strftime(datetimeString), startTimeStamp.strftime(datetimeString))
+
+cursor.execute(sql,var)
+result = cursor.fetchall()
+x = []
+y = []
+for i in result:
+    x.append([i[0]])
+    y.append(i[1])
+
+print(datetime.now())
+plt.plot(x, y, label="temp")
+print(datetime.now())
+plt.show()
+print(datetime.now())
+
+
