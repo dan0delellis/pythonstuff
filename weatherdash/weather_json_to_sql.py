392d009bdb459c878bd29653fd3cf830af6d7a94
Daniel De Lellis <dan.delellis@gmail.com>
2021-01-11 02:03:40 -0800

    WIP of openweathermap get

---
+++ weatherdash/weather_json_to_sql.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import datetime
+import time
+import sched
+import requests
+import json
+import mysql.connector
+
+s = sched.scheduler(time.time, time.sleep)
+
+sqlURL = "10.0.0.2"
+sqlUsr = "sensorsrecord"
+sqlPw = "donkeyboner"
+sqlDB = "climate"
+sqlTable = "json"
+
+db = mysql.connector.connect(host=sqlURL, user=sqlUsr, password=sqlPw, database=sqlDB)
+mycursor = db.cursor()
+
+cityID = open("cityid", "r").read().strip()
+apiKey = open("apikey", "r").read().strip()
+
+url = f"https://api.openweathermap.org/data/2.5/weather?id={cityID}&appid={apiKey}&units=standard"
+
+curl_response = requests.get(url)
+curl_response.encoding = 'utf-8'
+data = curl_response.text
+
+timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+query = f"INSERT into {sqlTable} (timestamp, location_code, report_json ) VALUES ( %s, %s, %s)"
+val = (timestamp, cityID, data)
+mycursor.execute(query, val)
+db.commit()
+
+
