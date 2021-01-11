19076bbf1ccaa45df13493535fd24867134b0f20
Daniel De Lellis <dan.delellis@gmail.com>
2021-01-11 02:24:08 -0800

    more WiP, records average of curled data because why not

--- weatherdash/weather_json_to_sql.py
+++ weatherdash/weather_json_to_sql.py
@@ -30,6 +30,17 @@ timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 query = f"INSERT into {sqlTable} (timestamp, location_code, report_json ) VALUES ( %s, %s, %s)"
 val = (timestamp, cityID, data)
 mycursor.execute(query, val)
+
+
+json = curl_response.json()
+temp = json["main"]["temp"] - 273.15
+humid = json["main"]["humidity"]
+pressure = json["main"]["pressure"]
+avgTS = json["dt"]
+location = json["name"]
+averagesQuery = "INSERT into averages (timestamp, location, temp, humid, pressure) VALUES (%s, %s, %s, %s, %s)"
+values = (avgTS, location, temp, humid, pressure)
+mycursor.execute(averagesQuery, values)
 db.commit()
 
 
